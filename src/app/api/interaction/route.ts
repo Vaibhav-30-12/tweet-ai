6985fb8b8af79b7fd5ec77cb56d48f1c64d01cd0
Vaibhav-30-12 <vaibhav16dabas@gmail.com>
2025-07-06 22:47:13 +0530

    route.ts interact updated

--- src/app/api/interaction/route.ts
+++ src/app/api/interaction/route.ts
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import next from "next";
 import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 
 export async function POST(req: Request) {
